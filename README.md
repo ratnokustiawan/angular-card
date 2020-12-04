130853a8ccc2ba3b1bd99b1df33b33de4beba0e1
ratnokustiawan <ratnokustiawan@gmail.com>
2020-12-04 13:37:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-4hunmv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-4hunmv)
---
+++ src/app/list-overview-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic list
+ */
+@Component({
+  selector: 'list-overview-example',
+  templateUrl: 'list-overview-example.html',
+})
+export class ListOverviewExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
